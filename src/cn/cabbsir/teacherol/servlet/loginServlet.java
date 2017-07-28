06ef851e041c14d7cdcabf5028d8b7263c503e5f
CabbSir <cabbsir@gmail.com>
2017-07-28 13:58:51 +0800

    servlet测试

---
+++ src/cn/cabbsir/teacherol/servlet/loginServlet.java
@@ -0,0 +1,32 @@
+package cn.cabbsir.teacherol.servlet;
+
+import cn.cabbsir.teacherol.entity.UserInfo;
+import cn.cabbsir.teacherol.logic.UserInfoDao;
+
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@javax.servlet.annotation.WebServlet(name = "loginServlet",urlPatterns = "/loginservlet")
+public class loginServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String identify = request.getParameter("member");
+        UserInfoDao dao =new UserInfoDao();
+        if(identify == "s"){
+            UserInfo loginUser = dao.loginSDao(username,password);
+            if(loginUser==null){
+                response.sendRedirect("login.jsp");
+            }
+            else{
+                HttpSession session =request.getSession();
+                session.setAttribute("loginuser",loginUser);
+                request.getRequestDispatcher("indexL.html");
+            }
+        }
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
